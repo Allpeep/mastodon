d2fb37b219eeb57c4419c2bd6d23c2a78a9a340d
Christoph Witzany <christoph@web.crofting.com>
2022-06-28 22:07:09 +0200

    Fix jam server url in serializer and js component

--- app/javascript/mastodon/features/jam/index.js
+++ app/javascript/mastodon/features/jam/index.js
@@ -74,7 +74,7 @@ class Jam extends ImmutablePureComponent {
       title={'Jam'}
       className={'jam__iframe'}
       allow='microphone;*'
-      src={`https://jam.systems/${jam.get('room_id')}?debug=yes#${jamHash}`}
+      src={`${jam.get('server_url')}${jam.get('room_id')}?debug=yes#${jamHash}`}
     />);
 
   }
