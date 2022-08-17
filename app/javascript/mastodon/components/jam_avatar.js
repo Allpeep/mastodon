cae52704077ec3631ffcf47829f951b75ef69170
Esatollah Ghazi <e.ghazi@student.tugraz.at>
2022-08-17 16:12:40 +0200

    adding avatar base

---
+++ app/javascript/mastodon/components/jam_avatar.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export const StageAvatar = ({
+    speaking,
+    canSpeak,
+    moderators,
+    peerId,
+    peerState,
+    reactions,
+    info,
+}) => {
+
+    let amSpeaking = speaking.has(peerId)
+
+
+    return (<li className='avatar-container' key={peerId} >
+        <div className={amSpeaking ? "speak-circle" : 'quiet-circle'}>
+            <img className='avatar' src={info.avatar} ></img>
+        </div>
+    </li >)
+
+}
