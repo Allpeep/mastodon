eaa94464c4418131f959425bbee21ed6beaef74a
Esatollah Ghazi <e.ghazi@student.tugraz.at>
2022-08-18 13:35:07 +0200

    importDefaultIdentity inside JamRoom component

--- app/javascript/mastodon/components/jam_room.js
+++ app/javascript/mastodon/components/jam_room.js
@@ -2,14 +2,27 @@ import React, { useEffect } from 'react';
 import { useJam, use } from 'jam-core-react';
 import Avatar from 'mastodon/components/avatar';
 import { StageAvatar } from './jam_avatar';
+import { importDefaultIdentity } from 'jam-core';
 
-const JamRoom = ({ roomId, handleleaveRoom }) => {
+const JamRoom = ({ roomId, handleleaveRoom, jam, account }) => {
   let [state, api] = useJam();
   let { enterRoom, leaveRoom, setProps } = api;
   let [myIdentity, speaking, peers, peerState, iSpeaker, reactions, identities, room, myPeerState] = use(state, ['myIdentity', 'speaking', 'peers', 'peerState', 'iAmSpeaker', 'reactions', 'identities', 'room', 'myPeerState']);
 
 
+
   useEffect(() => {
+
+    importDefaultIdentity(
+      {
+        info: {
+          name: account.get('display_name') || account.get('username'),
+          avatar: account.get('avatar_static'),
+        },
+        seed: jam.get('jam_seed'),
+      })
+
+
 
     async function enter() {
 
@@ -17,54 +30,51 @@ const JamRoom = ({ roomId, handleleaveRoom }) => {
       await setProps('roomId', roomId);
       await enterRoom(roomId);
 
-      return () => {
-        leaveRoom();
-      }
     }
     enter();
 
+    return () => {
+      leaveRoom();
+    }
   }, [])
 
   let {
-    speakers,
     moderators,
   } = room || {};
-
-
-
-
+  let speakers = account.get('speakers')
 
   return (
-    <div className='room-container'>
-      <button className='status__content__spoiler-link' onClick={handleleaveRoom}>Leave Room</button>
-      <img src={myIdentity.info.avatar}></img>
-      <div>{myIdentity.info.name}</div>
-      <div>{myIdentity.info.avatar}</div>
-      <ul className='speakerlist'>
-        {<StageAvatar
-          key={myIdentity.info.id}
-          peerId={myIdentity.info.id}
-          {...{ speaking, moderators, reactions, room }}
-          canSpeak={true}
-          peerState={myPeerState}
-          info={myIdentity.info}
-        />
-        }
+    <div>
+      <div><button className='status__content__spoiler-link jam-button' onClick={handleleaveRoom}>Leave Room</button></div>
+      <div className='room-container'>
+        <ul className='speakerlist'>
 
-        {(speakers ?? []).filter(id => peers.includes(id)).map(peerId => {
-
-          return <StageAvatar
-            key={peerId}
-            {...{ speaking }}
-            {...{ peerId, peerState, reactions }}
+          {<StageAvatar
+            key={myIdentity.info.id}
+            peerId={myIdentity.info.id}
+            {...{ speaking, moderators, reactions, room }}
             canSpeak={true}
-            peerState={peerState[peerId]}
-            info={identities[peerId]}
+            peerState={myPeerState}
+            info={myIdentity.info}
           />
+          }
 
-        })}
-      </ul>
+          {(speakers ?? []).filter(id => peers.includes(id)).map(peerId => {
 
+            return <StageAvatar
+              key={peerId}
+              {...{ speaking }}
+              {...{ peerId, peerState, reactions }}
+              canSpeak={true}
+              peerState={peerState[peerId]}
+              info={identities[peerId]}
+            />
+
+          })}
+
+        </ul>
+
+      </div>
     </div>
   );
 }
