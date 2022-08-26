da009bb8785004e806bf21638f75fc94a0fab703
Esatollah Ghazi <e.ghazi@student.tugraz.at>
2022-08-26 19:29:46 +0200

    Add Jam Form UI

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -22,6 +22,7 @@ import ImmutablePureComponent from 'react-immutable-pure-component';
 import { length } from 'stringz';
 import { countableText } from '../util/counter';
 import Icon from 'mastodon/components/icon';
+import JamFormContainer from '../containers/jam_form_container'
 
 const allowedAroundShortCode = '><\u0085\u0020\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029\u0009\u000a\u000b\u000c\u000d';
 
@@ -265,6 +266,7 @@ class ComposeForm extends ImmutablePureComponent {
           <div className='compose-form__modifiers'>
             <UploadFormContainer />
             <PollFormContainer />
+            <JamFormContainer />
           </div>
         </AutosuggestTextarea>
 
