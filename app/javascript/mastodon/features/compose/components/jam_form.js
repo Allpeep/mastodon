e5a9e2755f855abc7f67150c4474e1c761e24b15
Esatollah Ghazi <e.ghazi@student.tugraz.at>
2022-08-26 19:29:46 +0200

    Add Jam Form UI

---
+++ app/javascript/mastodon/features/compose/components/jam_form.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ImmutablePureComponent from 'react-immutable-pure-component';
+import { injectIntl } from 'react-intl';
+
+@injectIntl
+class JamForm extends ImmutablePureComponent {
+
+    static propTypes = {
+        jam: PropTypes.bool,
+        title: PropTypes.string,
+        onChangeTitle: PropTypes.func.isRequired,
+    };
+
+    handleChange = (e) => {
+        this.props.onChangeTitle(e.target.value);
+    };
+
+    render() {
+        const { jam, handleChange } = this.props;
+
+        if (!jam) {
+            return null;
+        }
+
+        return (
+            <div className='jam-form'>
+
+                <input placeholder='Jam room title' className='jam-form-input' value={this.props.title} onChange={this.handleChange} />
+
+            </div>
+        );
+    }
+
+}
+
+export default JamForm;
