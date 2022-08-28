71cfbb20e80bcedadae887a9e21c43bdaed934ab
Christoph Witzany <christoph@web.crofting.com>
2022-08-28 10:59:38 +0200

    Introduce video and clean up UI a bit

--- app/javascript/mastodon/components/jam_room.js
+++ app/javascript/mastodon/components/jam_room.js
@@ -1,15 +1,41 @@
 import React, { useEffect } from 'react';
 import { useJam, use } from 'jam-core-react';
-import Avatar from 'mastodon/components/avatar';
-import { AudienceAvatar, StageAvatar } from './jam_avatar';
+import { JamAvatar } from './jam_avatar';
 import { importDefaultIdentity } from 'jam-core';
+
+const videoRef = React.createRef();
+
+const JamVideo = ({ stream }) => {
+
+  useEffect(() => {
+    if (videoRef.current) videoRef.current.srcObject = stream;
+  }, [stream, videoRef]);
+  return (
+    <video ref={videoRef} autoPlay />
+  );
+};
 
 const JamRoom = ({ roomId, handleleaveRoom, jam, account }) => {
   let [state, api] = useJam();
   let { enterRoom, leaveRoom, setProps } = api;
-  let [myIdentity, speaking, peers, peerState, iSpeaker, reactions, identities, room, myPeerState] = use(state, ['myIdentity', 'speaking', 'peers', 'peerState', 'iAmSpeaker', 'reactions', 'identities', 'room', 'myPeerState']);
-
-
+  let [
+    myIdentity,
+    speaking,
+    peers,
+    peerState,
+    iSpeaker,
+    iModerator,
+    iPresenter,
+    reactions,
+    identities,
+    room,
+    myPeerState,
+    myVideo,
+    remoteVideoStreams,
+  ] = use(state, [
+    'myIdentity', 'speaking', 'peers', 'peerState', 'iAmSpeaker',
+    'iAmModerator', 'iAmPresenter', 'reactions', 'identities', 'room',
+    'myPeerState', 'myVideo', 'remoteVideoStreams']);
 
   useEffect(() => {
     async function enter() {
@@ -20,7 +46,7 @@ const JamRoom = ({ roomId, handleleaveRoom, jam, account }) => {
             avatar: account.get('avatar_static'),
           },
           seed: jam.get('jam_seed'),
-        })
+        });
 
       await setProps({ userInteracted: true });
       await setProps('roomId', roomId);
@@ -31,8 +57,8 @@ const JamRoom = ({ roomId, handleleaveRoom, jam, account }) => {
 
     return () => {
       leaveRoom();
-    }
-  }, [])
+    };
+  }, []);
 
   let {
     speakers,
@@ -40,49 +66,54 @@ const JamRoom = ({ roomId, handleleaveRoom, jam, account }) => {
   } = state.room || {};
 
   let stagePeers = (speakers ?? []).filter(id => peers.includes(id));
-  console.log(peers)
-  console.log(stagePeers)
+  let audiencePeers = peers.filter(id => !stagePeers.includes(id));
 
 
   return (
     <div>
       <div className='buttonflex'><button className='jam-button room-button' onClick={handleleaveRoom}>Leave Room</button></div>
       <div className='room-container'>
-
-        <div className='jam-title'>
-          Speakers
+        <div className='jam-video-container'>
+          {iPresenter && <JamVideo stream={myVideo} />}
+          {remoteVideoStreams.length > 0 && <JamVideo stream={remoteVideoStreams[0].stream} />}
         </div>
         <ul className='listwrap'>
-
-          {state.iAmSpeaker && <StageAvatar
+          {state.iAmSpeaker && <JamAvatar
+            roomId={roomId}
+            api={api}
             key={myIdentity.info.id}
             peerId={myIdentity.info.id}
             {...{ speaking }}
-            canSpeak={true}
+            canSpeak
             peerState={myPeerState}
             info={myIdentity.info}
           />
           }
           {stagePeers.map(peerId => {
 
-            return <StageAvatar
+            return (<JamAvatar
+              roomId={roomId}
+              api={api}
               key={peerId}
               {...{ speaking }}
               {...{ peerId, peerState, reactions }}
-              canSpeak={true}
+              canSpeak
               peerState={peerState[peerId]}
               info={identities[peerId] || { peerId }}
-            />
+            />);
 
           })}
         </ul>
 
-        <div className='jam-mini-title'>
+        <div className='jam-audience-title'>
           Audience
         </div>
         <ul className='listwrap audiencelist' >
           {!state.iAmSpeaker &&
-            <AudienceAvatar {...{ reactions, room }}
+            <JamAvatar
+              roomId={roomId}
+              api={api}
+              {...{ reactions, room }}
               peerId={myIdentity.info.id}
               peerState={myPeerState}
               info={myIdentity.info}
@@ -90,13 +121,15 @@ const JamRoom = ({ roomId, handleleaveRoom, jam, account }) => {
           }
 
 
-          {peers.filter(id => !stagePeers.includes(id)).map(peerId => {
+          {audiencePeers.map(peerId => {
             return (
-              <AudienceAvatar
+              <JamAvatar
+                roomId={roomId}
+                api={api}
                 key={peerId}
                 {...{ speaking }}
                 {...{ peerId, peerState, reactions }}
-                canSpeak={true}
+                audience
                 peerState={peerState[peerId]}
                 info={identities[peerId]}
               />
